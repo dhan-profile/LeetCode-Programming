5180dbe8c166961a5e547374b52b0a3417a522df
Dhanush <dhanushkrish54@gmail.com>
2024-04-03 18:30:05 +0530

    Time: 49 ms (5.02%) | Memory: 45 MB (5.20%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                // If the string becomes empty, it indicates all brackets are matched.
+                return s.isEmpty();
+            }
+        }
+    }
+}
