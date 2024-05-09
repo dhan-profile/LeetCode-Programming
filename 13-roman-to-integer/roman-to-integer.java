c167c87ba3c79280d073bd90bbad6fbfe2ae849c
Dhanush <dhanushkrish54@gmail.com>
2024-05-09 19:15:02 +0530

    Time: 2 ms (100.00%) | Memory: 44.2 MB (92.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,23 +1,19 @@
 class Solution {
-    public int romanToInt(String s) {
-        Map<Character,Integer> map = new HashMap<Character, Integer>();
-int sum=0;
-map.put('I',1);
-map.put('V',5);
-map.put('X',10);
-map.put('L',50);
-map.put('C',100);
-map.put('D',500);
-map.put('M',1000);
-s=s.replace("IV","IIII");
-s=s.replace("IX","VIIII");
-s=s.replace("XL","XXXX");
-s=s.replace("XC","LXXXX");
-s=s.replace("CD","CCCC");
-s=s.replace("CM","DCCCC");
-for(int i=0;i<s.length();i++){
-sum = sum + (map.get(s.charAt(i)));
-}
-return sum;
+ public int romanToInt(String s) {
+         int ans = 0, num = 0;
+        for (int i = s.length()-1; i >= 0; i--) {
+            switch(s.charAt(i)) {
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+            }
+            if (4 * num < ans) ans -= num;
+            else ans += num;
+        }
+        return ans;
     }
 }
