01f2cecb6df3b1a85be7caaa5f8e8acaa614efbb
Dhanush <dhanushkrish54@gmail.com>
2024-07-26 06:38:20 +0530

    Time: 7 ms (97.54%) | Memory: 44.2 MB (66.33%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,38 +1,81 @@
+// ============ Bellman Ford Algorithm ============
+// class Solution {
+//     public int findTheCity(int n, int[][] edges, int disThr) {
+//         int[] distance=new int[n];
+//         int[] counter=new int[n];
+
+//         for(int src=0;src<n;src++){ //for each city as source
+//             Arrays.fill(distance, 100000000);
+//             distance[src]=0;
+//             for(int iter=0;iter<n;iter++){ // for each city, iterate n-1 times
+
+//                 for(int [] arr: edges){
+//                     int u=arr[0];
+//                     int v=arr[1];
+//                     int wt=arr[2];
+
+//                     if(distance[u]!=100000000 && distance[u]+wt<distance[v]){
+//                         distance[v]=distance[u]+wt;
+//                     }
+//                     if(distance[v]!=100000000 && distance[v]+wt<distance[u]){
+//                         distance[u]=distance[v]+wt;
+//                     }
+//                 }
+//             }
+//             for(int i=0;i<n;i++){
+//                 if(i!=src && distance[i]<=disThr){
+//                     counter[src]++;
+//                 }
+//             }
+//         }
+      
+//         int res=0;
+//         int highPath=100000000;
+//         for(int i=0;i<n;i++){
+//             if(counter[i]<=highPath){
+//                 highPath=counter[i];
+//                 res=i;
+//             }
+//         }
+// return res;
+//     }
+// }
+
+// ============ Floyd warshall Algorithm ==============
 class Solution {
     public int findTheCity(int n, int[][] edges, int disThr) {
-        int[] distance=new int[n];
-        int[] counter=new int[n];
+        int[][] distance=new int[n][n];
+        for(int [] arr: distance){
+        Arrays.fill(arr, 100000000);
+        }
+        for(int[] arr: edges){
+            distance[arr[0]][arr[1]]=arr[2];
+            distance[arr[1]][arr[0]]=arr[2];
+        }
 
-        for(int src=0;src<n;src++){ //for each city as source
-            Arrays.fill(distance, 100000000);
-            distance[src]=0;
-            for(int iter=0;iter<n;iter++){ // for each city, iterate n-1 times
+        for(int i=0;i<n;i++){
+            distance[i][i]=0;
+        }
 
-                for(int [] arr: edges){
-                    int u=arr[0];
-                    int v=arr[1];
-                    int wt=arr[2];
-
-                    if(distance[u]!=100000000 && distance[u]+wt<distance[v]){
-                        distance[v]=distance[u]+wt;
-                    }
-                    if(distance[v]!=100000000 && distance[v]+wt<distance[u]){
-                        distance[u]=distance[v]+wt;
-                    }
-                }
-            }
+        for(int viaNode=0;viaNode<n;viaNode++){
             for(int i=0;i<n;i++){
-                if(i!=src && distance[i]<=disThr){
-                    counter[src]++;
+                for(int j=0;j<n;j++){
+                    distance[i][j]=Math.min(distance[i][j], distance[i][viaNode]+distance[viaNode][j]);
                 }
             }
         }
-      
+        
         int res=0;
-        int highPath=100000000;
+        int occurences=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            if(counter[i]<=highPath){
-                highPath=counter[i];
+            int tempC=0;
+            for(int j=0;j<n;j++){
+                if(i!=j && distance[i][j] <= disThr ){
+                    ++tempC;
+                }
+            }
+            if(tempC<=occurences){
+                occurences=tempC;
                 res=i;
             }
         }
