6bfd8a6a04121b67d12cc889ff7ae6d93fbc83a0
Dhanush <dhanushkrish54@gmail.com>
2024-08-03 09:59:08 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (42.83%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -11,24 +11,25 @@
 // }
 
 class Solution {
-    public boolean canBeEqual(int[] target, int[] arr) {
-        int n = target.length, m = arr.length;
-
-        if (n != m) return false;
-
-        int[] map = new int[1001];
-
-        for (int i = 0; i < n; i++) {
-            map[target[i]]++;
-            map[arr[i]]--;
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (map[target[i]] != 0 || map[arr[i]] != 0) {
-                return false;
-            }
-        }
-
-        return true;
+   public boolean canBeEqual(int[] target, int[] arr) {
+    if (target.length != arr.length) {
+        return false;
     }
+
+    int[] map = new int[1001]; // assuming elements are within the range [0, 1000]
+
+    for (int i = 0; i < target.length; i++) {
+        map[target[i]]++;
+        map[arr[i]]--;
+    }
+
+    for (int count : map) {
+        if (count != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 }
