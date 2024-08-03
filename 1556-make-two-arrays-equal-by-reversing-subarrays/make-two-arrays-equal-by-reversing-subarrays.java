ff87440fb19c666f05fd9bb80ab375c9742a3a0d
Dhanush <dhanushkrish54@gmail.com>
2024-08-03 09:54:07 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (56.26%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,11 +1,34 @@
+// class Solution {
+//     public boolean canBeEqual(int[] target, int[] arr) {
+//         if (target.length != arr.length) {
+//             return false;
+//         } else {
+//             Arrays.sort(target);
+//             Arrays.sort(arr);
+//         }
+//         return Arrays.equals(target, arr);
+//     }
+// }
+
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        if (target.length != arr.length) {
-            return false;
-        } else {
-            Arrays.sort(target);
-            Arrays.sort(arr);
+        int n = target.length, m = arr.length;
+
+        if (n != m) return false;
+
+        int[] map = new int[1001];
+
+        for (int i = 0; i < n; i++) {
+            map[target[i]]++;
+            map[arr[i]]--;
         }
-        return Arrays.equals(target, arr);
+
+        for (int i = 0; i < n; i++) {
+            if (map[target[i]] != 0 || map[arr[i]] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
